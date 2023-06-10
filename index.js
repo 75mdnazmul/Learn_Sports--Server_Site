5aa25ea9b2285c02aabb3944b2baf84454a9ca6d
75mdnazmul <75mdnazmulhasan@gmail.com>
2023-06-10 22:19:23 +0600

    Cors is installed and server is runing

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 // MiddleWare
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res)=>{
     res.send('Learning sports is starting')
